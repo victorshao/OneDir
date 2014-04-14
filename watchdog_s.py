ab8c9c99b0b7d340a6fe9e7fecb366908c48c803
PeterShao <pds9vc@virginia.edu>
2014-04-14 01:34:46 -0400

    Update 4/14/14

--- watchdog_s.py
+++ watchdog_s.py
@@ -4,18 +4,27 @@ import shutil
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
+import unicodedata
+import string
+validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+def removeDisallowedFilenameChars(filename):
+    cleanedFilename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore')
+    return ''.join(c for c in cleanedFilename if c in validFilenameChars)
 class OneDirHandler(FileSystemEventHandler):
-
     def on_created(self, event):
         url = 'http://127.0.0.1:5000/upload'
         files = {'file': open(event.src_path,'r+')}
         r = requests.post(url,files=files)
 
 
-    # def on_deleted(self, event):
-    #
-    #
+    def on_deleted(self, event):
+        url = 'http://127.0.0.1:5000/delete/'
+        files = os.path.basename(event.src_path)
+        files = removeDisallowedFilenameChars(files)
+        files = files.replace(" ", "_")
+        url=url+files
+        r= requests.post(url)
+
     # def on_modified(self, event):
     #
     #
