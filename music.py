119b9bf14918cddde4082befe3746ac50eb333a0
PeterShao <pds9vc@virginia.edu>
2014-04-25 19:37:03 -0400

    updated again

--- music.py
+++ music.py
@@ -3,9 +3,12 @@ class start:
     def __init__(self):
         song = pyglet.media.load('start.wav')
         song.play()
-        pyglet.app.run()
+        # pyglet.app.run()
+
 class exit:
     def __init__(self):
         song = pyglet.media.load('exit.wav')
         song.play()
-        pyglet.app.run()
+        # pyglet.app.run()
+
+
